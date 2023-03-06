918b32e3f845d239ee7ac80b6a42d3b9448acccb
Aurelia Villyani <aurel.villyani@students.pcci.edu>
2023-03-05 20:30:15 -0600

    finished

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Oracle.ManagedDataAccess.Client;
@@ -23,13 +23,17 @@ namespace DOOM.Pages
             con.Open();
             OracleCommand cmd = con.CreateCommand();
             cmd.CommandText = @"DECLARE
-                                 CURSOR c_instructors IS SELECT * FROM instructor WHERE last_name LIKE '%' || :last_name || '%'; 
+                                 CURSOR c_instructors IS SELECT * FROM instructor WHERE last_name LIKE '%' || :last_name_pattern || '%'; 
                                  v_count NUMBER(3,0) := 0; 
                                BEGIN  
                                  FOR v_instructor IN c_instructors LOOP  
                                      v_count := v_count + 1;    
-                                 END LOOP;  
-                                 raise_application_error(-20000, 'Number os the instructor with ' || :last_name || ' is ' || v_count);
+                                 END LOOP;
+                                 IF v_count = 0 THEN
+                                    raise_application_error(-20000, 'No instructors found with the given search criteria.');
+                                 ELSE
+                                    raise_application_error(-20000, 'Number of instructors with ' || :last_name_pattern || ' in their last name: ' || v_count);
+                                 END IF;
                                END;";
             try
             {
